c7a57c80c1a8cb62142aa24d8adf8610ebb4942e
at3158 <tk290@tkfood.com.tw>
2016-08-11 16:19:11 +0800

    MODIFER EDI

--- TKWAREHOUSE/FrmhctEDIExport.Designer.cs
+++ TKWAREHOUSE/FrmhctEDIExport.Designer.cs
@@ -196,6 +196,7 @@
             this.button2.TabIndex = 23;
             this.button2.Text = "匯出";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // FrmhctEDIExport
             // 
