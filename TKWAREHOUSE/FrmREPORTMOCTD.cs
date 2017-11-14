e1f380ea5b21a8d52aead0ec0b814028fda47b07
at3158 <tk290@tkfood.com.tw>
2017-11-14 16:09:22 +0800

    ADD MOCTE

---
+++ TKWAREHOUSE/FrmREPORTMOCTD.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using System.Text.RegularExpressions;
+using FastReport;
+using FastReport.Data;
+
+namespace TKWAREHOUSE
+{
+    public partial class FrmREPORTMOCTD : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataSet ds2 = new DataSet();
+        string tablename = null;
+        int rownum = 0;
+        DataGridViewRow row;
+        string SALSESID = null;
+        int result;
+
+        public FrmREPORTMOCTD()
+        {
+            InitializeComponent();
+            combobox2load();
+        }
+
+        #region FUNCTION
+        public void combobox2load()
+        {
+
+            connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+            sqlConn = new SqlConnection(connectionString);
+            String Sequel = "SELECT MD002,MD001 FROM [TK].dbo.CMSMD WHERE MD002 LIKE '新%' ORDER BY MD001";
+            SqlDataAdapter da = new SqlDataAdapter(Sequel, sqlConn);
+            DataTable dt = new DataTable();
+            sqlConn.Open();
+
+            dt.Columns.Add("MD001", typeof(string));
+            dt.Columns.Add("MD002", typeof(string));
+            da.Fill(dt);
+            comboBox2.DataSource = dt.DefaultView;
+            comboBox2.ValueMember = "MD001";
+            comboBox2.DisplayMember = "MD002";
+            sqlConn.Close();
+
+           
+
+        }
+
+        public void SETFASTREPORT()
+        {
+            //string SQL;
+            //report1 = new Report();
+            //report1.Load(@"REPORT\銷售預估.frx");
+
+            //report1.Dictionary.Connections[0].ConnectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+
+            //TableDataSource Table = report1.GetDataSource("Table") as TableDataSource;
+            //SQL = SETFASETSQL();
+            //Table.SelectCommand = SQL;
+            //report1.Preview = previewControl1;
+            //report1.Show();
+
+
+        }
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+
+    }
+}
