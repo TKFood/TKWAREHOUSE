24fce5e4da9d0f53978f8437e41aafb7b6523c14
at3158 <tk290@tkfood.com.tw>
2017-12-14 16:09:38 +0800

    ADDTOTKWAREHOUSE

--- TKWAREHOUSE/FrmMOCINV.cs
+++ TKWAREHOUSE/FrmMOCINV.cs
@@ -37,13 +37,25 @@ namespace TKWAREHOUSE
         SqlCommand cmd = new SqlCommand();
         DataSet ds = new DataSet();
         DataSet ds2 = new DataSet();
+        DataSet ds3 = new DataSet();
 
         DataTable dt = new DataTable();
         string tablename = null;
 
+        DataTable ADDDT = new DataTable();
+
+
         public FrmMOCINV()
         {
             InitializeComponent();
+
+            
+            ADDDT.Columns.AddRange(new DataColumn[5] {
+                 new DataColumn("日期", typeof(string)),
+                 new DataColumn("品號", typeof(string)),
+                 new DataColumn("品名", typeof(string)),
+                 new DataColumn("批號", typeof(string)),
+                 new DataColumn("數量", typeof(decimal)) });
         }
 
         #region FUNCTION
@@ -156,7 +168,8 @@ namespace TKWAREHOUSE
             STR.AppendFormat(@"  FROM [TK].dbo.MOCTA,[TK].dbo.MOCTB");
             STR.AppendFormat(@"  WHERE TA001=TB001 AND TA002=TB002");
             STR.AppendFormat(@"  AND TA003>='{0}' AND TA003<='{1}' ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
-            STR.AppendFormat(@"  AND TB003='101001001'");
+            //STR.AppendFormat(@"  AND TB003='101002001'");
+            STR.AppendFormat(@"  AND( TB003 LIKE '1%' OR TB003 LIKE '2%')");
             STR.AppendFormat(@"  GROUP BY TB003,TB012");
             STR.AppendFormat(@"  ORDER BY TB003,TB012");
             STR.AppendFormat(@"  ");
@@ -183,19 +196,19 @@ namespace TKWAREHOUSE
 
                     sbSql.Clear();
 
-                    sbSql.AppendFormat(@" SELECT LA009 AS '倉庫',LA016  AS '批號',SUM(LA005*LA011)  AS '庫存量' ");
-                    sbSql.AppendFormat(@" FROM INVLA");
-                    sbSql.AppendFormat(@" WHERE LA009='20006'");
+                    sbSql.AppendFormat(@" SELECT LA009 AS '倉庫',LA001 AS '品號',LA016  AS '批號',SUM(LA005*LA011)  AS '庫存量' ");
+                    sbSql.AppendFormat(@" FROM [TK].dbo.INVLA");
+                    sbSql.AppendFormat(@" WHERE (LA009='20006' OR LA009='20004')");
                     sbSql.AppendFormat(@" AND LA001 IN (SELECT TB003");
                     sbSql.AppendFormat(@" FROM [TK].dbo.MOCTA,[TK].dbo.MOCTB");
                     sbSql.AppendFormat(@" WHERE TA001=TB001 AND TA002=TB002");
                     sbSql.AppendFormat(@"  AND TA003>='{0}' AND TA003<='{1}' ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
-                    sbSql.AppendFormat(@" AND TB003='101001001'");
+                    //sbSql.AppendFormat(@" AND TB003='101002001'");
                     sbSql.AppendFormat(@" GROUP BY TB003");
                     sbSql.AppendFormat(@" )");
-                    sbSql.AppendFormat(@" GROUP BY LA009,LA016");
+                    sbSql.AppendFormat(@" GROUP BY LA009,LA001,LA016");
                     sbSql.AppendFormat(@" HAVING  SUM(LA005*LA011)>0");
-                    sbSql.AppendFormat(@" ORDER BY LA009,LA016");
+                    sbSql.AppendFormat(@" ORDER BY LA009,LA001,LA016");
                     sbSql.AppendFormat(@" ");
 
 
@@ -237,6 +250,164 @@ namespace TKWAREHOUSE
 
             }
         }
+
+
+        public void SearchINVBATCH()
+        {
+            string ID = dateTimePicker1.Value.ToString("yyyyMMdd");
+            ADDDT.Clear();
+
+            if (dataGridView1.Rows.Count>=1)
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    string MB001 = row.Cells["品號"].Value.ToString();
+                    decimal NUM= Convert.ToDecimal(row.Cells["數量"].Value.ToString());
+                    ds3.Clear();
+                    ds3 = SearchINVNOW(MB001);
+
+                    if(ds3.Tables["ds3"].Rows.Count>=1)
+                    {
+                        int ROWS = ds3.Tables["ds3"].Rows.Count;
+                        int NOWROWS = ds3.Tables["ds3"].Rows.Count;
+
+                        while (NUM>0 && NOWROWS >= 1)
+                        {
+                            if(Convert.ToDecimal(ds3.Tables["ds3"].Rows[ROWS - NOWROWS]["庫存量"].ToString())>= NUM)
+                            {
+                                ADDDT.Rows.Add(ID,row.Cells["品號"].Value.ToString(), row.Cells["品名"].Value.ToString(), ds3.Tables["ds3"].Rows[ROWS - NOWROWS]["批號"].ToString(), NUM);
+                            }
+                            else if (Convert.ToDecimal(ds3.Tables["ds3"].Rows[ROWS - NOWROWS]["庫存量"].ToString()) < NUM)
+                            {
+                                ADDDT.Rows.Add(ID, row.Cells["品號"].Value.ToString(), row.Cells["品名"].Value.ToString(), ds3.Tables["ds3"].Rows[ROWS - NOWROWS]["批號"].ToString(), Convert.ToDecimal(ds3.Tables["ds3"].Rows[ROWS - NOWROWS]["庫存量"].ToString()));
+                            }
+
+                            NUM=NUM- Convert.ToDecimal(ds3.Tables["ds3"].Rows[ROWS- NOWROWS]["庫存量"].ToString());
+                            NOWROWS = NOWROWS - 1;
+                        }
+                       
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+            }
+
+            ADDTOTKWAREHOUSE();
+
+
+        }
+
+        public DataSet SearchINVNOW(string MB001)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(dateTimePicker1.Value.ToString("MB001")) )
+                {
+                    connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                    sqlConn = new SqlConnection(connectionString);
+
+                    sbSql.Clear();
+
+                    sbSql.AppendFormat(@" SELECT LA009 AS '倉庫',LA001 AS '品號',LA016  AS '批號',SUM(LA005*LA011)  AS '庫存量'  ");
+                    sbSql.AppendFormat(@" FROM INVLA");
+                    sbSql.AppendFormat(@" WHERE (LA009='20006' OR LA009='20004')");
+                    sbSql.AppendFormat(@" AND LA001 IN (SELECT TB003");
+                    sbSql.AppendFormat(@" FROM [TK].dbo.MOCTA,[TK].dbo.MOCTB");
+                    sbSql.AppendFormat(@" WHERE TA001=TB001 AND TA002=TB002");
+                    sbSql.AppendFormat(@"  AND TA003>='{0}' AND TA003<='{1}' ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+                    sbSql.AppendFormat(@" AND TB003='{0}'", MB001);
+                    sbSql.AppendFormat(@" GROUP BY TB003");
+                    sbSql.AppendFormat(@" )");
+                    sbSql.AppendFormat(@" GROUP BY LA009,LA001,LA016");
+                    sbSql.AppendFormat(@" HAVING  SUM(LA005*LA011)>0");
+                    sbSql.AppendFormat(@" ORDER BY LA009,LA001,LA016");
+                    sbSql.AppendFormat(@" ");
+
+
+
+                    adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+                    sqlCmdBuilder = new SqlCommandBuilder(adapter);
+
+                    sqlConn.Open();
+                    ds3.Clear();
+                    adapter.Fill(ds3, "ds3");
+                    sqlConn.Close();
+
+
+                    if (ds2.Tables[tablename].Rows.Count == 0)
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        return ds3;
+
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+
+
+
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+
+            }
+        }
+
+        public void ADDTOTKWAREHOUSE()
+        {
+            if (ADDDT.Rows.Count >= 1)
+            {
+                dataGridView3.DataSource = ADDDT;
+
+                try
+                {
+                    connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
+                        {
+                            //Set the database table name
+                            sqlBulkCopy.DestinationTableName = "[TKWAREHOUSE].[dbo].[INVBATCH]";
+
+                            //[OPTIONAL]: Map the DataTable columns with that of the database table
+                            sqlBulkCopy.ColumnMappings.Add("日期", "ID");
+                            sqlBulkCopy.ColumnMappings.Add("品號", "MB001");
+                            sqlBulkCopy.ColumnMappings.Add("品名", "MB002");
+                            sqlBulkCopy.ColumnMappings.Add("批號", "LOTNO");
+                            sqlBulkCopy.ColumnMappings.Add("數量", "NUM");
+
+
+                            con.Open();
+                            //sqlBulkCopy.WriteToServer(ADDDT);
+                            con.Close();
+
+                            MessageBox.Show("新增完成");
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("新增錯誤");
+                }
+                finally
+                {
+
+                }
+             }
+        }
+      
         #endregion
 
         #region BUTTON
@@ -251,6 +422,11 @@ namespace TKWAREHOUSE
         private void button2_Click(object sender, EventArgs e)
         {
             SearchINV();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SearchINVBATCH();
         }
     }
 }
