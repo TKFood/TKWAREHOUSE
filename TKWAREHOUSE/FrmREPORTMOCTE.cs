71d074862dcac35a0f23b81e61e308ed603c2021
at3158 <tk290@tkfood.com.tw>
2018-01-18 15:53:11 +0800

    re INLVA

--- TKWAREHOUSE/FrmREPORTMOCTE.cs
+++ TKWAREHOUSE/FrmREPORTMOCTE.cs
@@ -100,45 +100,78 @@ namespace TKWAREHOUSE
 
             if(comboBox1.Text.ToString().Equals("原料"))
             {
-                FASTSQL.AppendFormat(@" SELECT MD002,TE004,TE017 ,TE011,TE012,SUM(LA005*LA011)*-1  AS TE005,TE010 ");
-                FASTSQL.AppendFormat(@" FROM [TK].dbo.CMSMD, [TK].dbo.MOCTC,[TK].dbo.MOCTE,[TK].dbo.INVLA");
+                FASTSQL.AppendFormat(@" SELECT MD002,TE004,TE017 ,TE011,TE012,SUM(TE005)  AS TE005,TE010 ");
+                FASTSQL.AppendFormat(@" FROM [TK].dbo.CMSMD, [TK].dbo.MOCTC,[TK].dbo.MOCTE");
                 FASTSQL.AppendFormat(@" WHERE MD002 LIKE '新%' ");
                 FASTSQL.AppendFormat(@" AND MD001=TC005 ");
                 FASTSQL.AppendFormat(@" AND TC001=TE001 AND TC002=TE002 ");
-                FASTSQL.AppendFormat(@" AND LA006=TE001 AND LA007=TE002 AND LA008=TE003");
                 FASTSQL.AppendFormat(@" AND ((TE004 LIKE '1%' ) OR (TE004 LIKE '3010000%' AND LEN(TE004)=10))   ");
                 FASTSQL.AppendFormat(@" AND TC003>={0}  AND TC003<={1} ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
                 FASTSQL.AppendFormat(@" AND MD002='{0}' ", comboBox2.Text.ToString());
                 FASTSQL.AppendFormat(@" GROUP BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
                 FASTSQL.AppendFormat(@" ORDER BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
+
+                //FASTSQL.AppendFormat(@" SELECT MD002,TE004,TE017 ,TE011,TE012,SUM(LA005*LA011)*-1  AS TE005,TE010 ");
+                //FASTSQL.AppendFormat(@" FROM [TK].dbo.CMSMD, [TK].dbo.MOCTC,[TK].dbo.MOCTE,[TK].dbo.INVLA");
+                //FASTSQL.AppendFormat(@" WHERE MD002 LIKE '新%' ");
+                //FASTSQL.AppendFormat(@" AND MD001=TC005 ");
+                //FASTSQL.AppendFormat(@" AND TC001=TE001 AND TC002=TE002 ");
+                //FASTSQL.AppendFormat(@" AND LA006=TE001 AND LA007=TE002 AND LA008=TE003");
+                //FASTSQL.AppendFormat(@" AND ((TE004 LIKE '1%' ) OR (TE004 LIKE '3010000%' AND LEN(TE004)=10))   ");
+                //FASTSQL.AppendFormat(@" AND TC003>={0}  AND TC003<={1} ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+                //FASTSQL.AppendFormat(@" AND MD002='{0}' ", comboBox2.Text.ToString());
+                //FASTSQL.AppendFormat(@" GROUP BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
+                //FASTSQL.AppendFormat(@" ORDER BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
             }
             else if (comboBox1.Text.ToString().Equals("物料"))
             {
-                FASTSQL.AppendFormat(@" SELECT MD002,TE004,TE017 ,TE011,TE012,SUM(LA005*LA011)*-1  AS TE005,TE010 ");
-                FASTSQL.AppendFormat(@" FROM [TK].dbo.CMSMD, [TK].dbo.MOCTC,[TK].dbo.MOCTE,[TK].dbo.INVLA ");
+                FASTSQL.AppendFormat(@" SELECT MD002,TE004,TE017 ,TE011,TE012,SUM(TE005)  AS TE005,TE010 ");
+                FASTSQL.AppendFormat(@" FROM [TK].dbo.CMSMD, [TK].dbo.MOCTC,[TK].dbo.MOCTE ");
                 FASTSQL.AppendFormat(@" WHERE MD002 LIKE '新%' ");
                 FASTSQL.AppendFormat(@" AND MD001=TC005 ");
                 FASTSQL.AppendFormat(@" AND TC001=TE001 AND TC002=TE002 ");
-                FASTSQL.AppendFormat(@" AND LA006=TE001 AND LA007=TE002 AND LA008=TE003");
                 FASTSQL.AppendFormat(@" AND TE004 LIKE '2%' ");
                 FASTSQL.AppendFormat(@" AND TC003>={0}  AND TC003<={1} ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
                 FASTSQL.AppendFormat(@" AND MD002='{0}' ", comboBox2.Text.ToString());
                 FASTSQL.AppendFormat(@" GROUP BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
                 FASTSQL.AppendFormat(@" ORDER BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
+
+                //FASTSQL.AppendFormat(@" SELECT MD002,TE004,TE017 ,TE011,TE012,SUM(LA005*LA011)*-1  AS TE005,TE010 ");
+                //FASTSQL.AppendFormat(@" FROM [TK].dbo.CMSMD, [TK].dbo.MOCTC,[TK].dbo.MOCTE,[TK].dbo.INVLA ");
+                //FASTSQL.AppendFormat(@" WHERE MD002 LIKE '新%' ");
+                //FASTSQL.AppendFormat(@" AND MD001=TC005 ");
+                //FASTSQL.AppendFormat(@" AND TC001=TE001 AND TC002=TE002 ");
+                //FASTSQL.AppendFormat(@" AND LA006=TE001 AND LA007=TE002 AND LA008=TE003");
+                //FASTSQL.AppendFormat(@" AND TE004 LIKE '2%' ");
+                //FASTSQL.AppendFormat(@" AND TC003>={0}  AND TC003<={1} ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+                //FASTSQL.AppendFormat(@" AND MD002='{0}' ", comboBox2.Text.ToString());
+                //FASTSQL.AppendFormat(@" GROUP BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
+                //FASTSQL.AppendFormat(@" ORDER BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
             }
             else if (comboBox1.Text.ToString().Equals("原料+物料"))
             {
-                FASTSQL.AppendFormat(@" SELECT MD002,TE004,TE017 ,TE011,TE012,SUM(LA005*LA011)*-1 AS TE005,TE010 ");
-                FASTSQL.AppendFormat(@" FROM [TK].dbo.CMSMD, [TK].dbo.MOCTC,[TK].dbo.MOCTE,[TK].dbo.INVLA ");
+                FASTSQL.AppendFormat(@" SELECT MD002,TE004,TE017 ,TE011,TE012,SUM(TE005) AS TE005,TE010 ");
+                FASTSQL.AppendFormat(@" FROM [TK].dbo.CMSMD, [TK].dbo.MOCTC,[TK].dbo.MOCTE");
                 FASTSQL.AppendFormat(@" WHERE MD002 LIKE '新%' ");
                 FASTSQL.AppendFormat(@" AND MD001=TC005 ");
                 FASTSQL.AppendFormat(@" AND TC001=TE001 AND TC002=TE002 ");
-                FASTSQL.AppendFormat(@" AND LA006=TE001 AND LA007=TE002 AND LA008=TE003");
                 FASTSQL.AppendFormat(@" AND (TE004 LIKE '1%' OR TE004 LIKE '2%' OR (TE004 LIKE '3010000%' AND LEN(TE004)=10))");
                 FASTSQL.AppendFormat(@" AND TC003>={0}  AND TC003<={1} ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
                 FASTSQL.AppendFormat(@" AND MD002='{0}' ", comboBox2.Text.ToString());
                 FASTSQL.AppendFormat(@" GROUP BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
                 FASTSQL.AppendFormat(@" ORDER BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
+
+                //FASTSQL.AppendFormat(@" SELECT MD002,TE004,TE017 ,TE011,TE012,SUM(LA005*LA011)*-1 AS TE005,TE010 ");
+                //FASTSQL.AppendFormat(@" FROM [TK].dbo.CMSMD, [TK].dbo.MOCTC,[TK].dbo.MOCTE,[TK].dbo.INVLA ");
+                //FASTSQL.AppendFormat(@" WHERE MD002 LIKE '新%' ");
+                //FASTSQL.AppendFormat(@" AND MD001=TC005 ");
+                //FASTSQL.AppendFormat(@" AND TC001=TE001 AND TC002=TE002 ");
+                //FASTSQL.AppendFormat(@" AND LA006=TE001 AND LA007=TE002 AND LA008=TE003");
+                //FASTSQL.AppendFormat(@" AND (TE004 LIKE '1%' OR TE004 LIKE '2%' OR (TE004 LIKE '3010000%' AND LEN(TE004)=10))");
+                //FASTSQL.AppendFormat(@" AND TC003>={0}  AND TC003<={1} ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+                //FASTSQL.AppendFormat(@" AND MD002='{0}' ", comboBox2.Text.ToString());
+                //FASTSQL.AppendFormat(@" GROUP BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
+                //FASTSQL.AppendFormat(@" ORDER BY MD002,TE004,TE017 ,TE011,TE012,TE010 ");
             }
 
 
