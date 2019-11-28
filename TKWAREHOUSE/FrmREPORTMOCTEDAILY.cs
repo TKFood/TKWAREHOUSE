14dfe9feabd6dca7167fb9264d6fea95e393b1f1
at3158 <tk290@tkfood.com.tw>
2019-11-28 17:20:13 +0800

    SETFASETSQL

--- TKWAREHOUSE/FrmREPORTMOCTEDAILY.cs
+++ TKWAREHOUSE/FrmREPORTMOCTEDAILY.cs
@@ -75,9 +75,11 @@ namespace TKWAREHOUSE
             StringBuilder STRQUERY = new StringBuilder();
 
             FASTSQL.AppendFormat(@"  SELECT TC003 AS '日期',TE004 AS '品號',TE017 AS '品名',SUM(MQ010*TE005)*-1 AS '總領退量',TE006 AS '單位'");
+            FASTSQL.AppendFormat(@"  ,(SELECT ISNULL(SUM(LA005*LA011),0) FROM [TK].dbo.INVLA WHERE LA001=TE004 AND LA009 IN ('20004','20006')) AS '庫存量'");
             FASTSQL.AppendFormat(@"  FROM  [TK].dbo.CMSMQ,[TK].dbo.MOCTC,[TK].dbo.MOCTE");
             FASTSQL.AppendFormat(@"  WHERE TC001=TE001 AND TC002=TE002 AND MQ001=TC001");
             FASTSQL.AppendFormat(@"  AND (TE004 LIKE '1%' OR TE004 LIKE '2%' )");
+            FASTSQL.AppendFormat(@"  AND TC009='Y'");
             FASTSQL.AppendFormat(@"  AND TC003>='{0}' AND TC003<='{1}'",dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
             FASTSQL.AppendFormat(@"  GROUP BY TC003,TE004,TE017,TE006");
             FASTSQL.AppendFormat(@"  ORDER BY TC003,TE004,TE017,TE006");
