e8a68bd3388063be6ed33cc026283eb15451b577
at3158 <tk290@tkfood.com.tw>
2020-10-07 11:37:12 +0800

    SETFASETSQL2

--- TKWAREHOUSE/FrmREPORTMOC.cs
+++ TKWAREHOUSE/FrmREPORTMOC.cs
@@ -106,20 +106,27 @@ namespace TKWAREHOUSE
 
           
             FASTSQL.AppendFormat(@"    
-                                  SELECT MD002 AS '線別',TA001 AS '製令單別',TA002 AS '製令單號',TA003 AS '開單日期',TA006 AS '產品品號',TA034 AS '產品品名',TA015 AS '預計產量',TA007 AS '單位1',TB003 AS '材料品號',TB012 AS '材料品名',TB007 AS '單位2',TB004 AS '需領用量',MC004 AS '標準批量',ROUND(TA015/MC004,2) AS '總桶數'
-                                  ,CASE WHEN FLOOR(ROUND(TA015/MC004,2)) = ROUND(TA015/MC004,2) THEN ROUND(TA015/MC004,2) ELSE CASE WHEN (ROUND(TA015/MC004,2)-1)>0 THEN FLOOR(ROUND(TA015/MC004,2)) ELSE 0 END  END AS '整桶數'
-                                  ,CASE WHEN FLOOR(ROUND(TA015/MC004,2)) != ROUND(TA015/MC004,2) THEN 1 ELSE 0  END AS '最後桶數'
-                                  ,CASE WHEN FLOOR(ROUND(TA015/MC004,2)) = ROUND(TA015/MC004,2) THEN ROUND(TB004/ROUND(TA015/MC004,2),2) ELSE CASE WHEN (ROUND(TA015/MC004,2)-1)>0 THEN ROUND(TB004/ROUND(TA015/MC004,2),2) ELSE 0 END  END AS '整桶用量'
-                                  ,TB004-(ROUND(TB004/ROUND(TA015/MC004,2),2)*(CASE WHEN FLOOR(ROUND(TA015/MC004,2)) = ROUND(TA015/MC004,2) THEN ROUND(TA015/MC004,2) ELSE CASE WHEN (ROUND(TA015/MC004,2)-1)>0 THEN FLOOR(ROUND(TA015/MC004,2)) ELSE 0 END  END)) AS '最後桶用量'
-                                  ,ROUND(TB004/ROUND(TA015/MC004,2),2)  AS '標準用量'
-                                  FROM [TK].dbo.MOCTA,[TK].dbo.MOCTB,[TK].dbo.BOMMC,[TK].dbo.CMSMD
-                                  WHERE TA001=TB001 AND TA002=TB002
-                                  AND TA006=MC001
-                                  AND TA021=MD001
-                                  AND TB003 LIKE '1%'
-                                  AND TB003 NOT IN ('101001001','101001009')
-                                  AND MD002  IN ('新廠製一組','新廠製二組','新廠製三組(手工)') 
-                                  AND TA003>='{0}' AND TA003<='{1}'"
+                                    SELECT 線別,製令單別,製令單號,開單日期,產品品號,產品品名,預計產量,單位1,材料品號,材料品名,單位2,需領用量,標準批量,總桶數,整桶數,最後桶數,整桶用量,最後桶用量,標準用量
+                                    ,ISNULL(BOMMD.MD004,'') AS '材料單位',ISNULL(BOMMD.MD006,0) AS '組成用量',ISNULL(BOMMD.MD007,0) AS '底數',ISNULL(BOMMD.MD008,0) AS '損耗率%'
+                                    ,CASE WHEN ISNULL(BOMMD.MD006,0)>0 THEN ISNULL(BOMMD.MD006,0)/ISNULL(BOMMD.MD007,0)*(1+ISNULL(BOMMD.MD008,0)) ELSE 0 END AS 'BOM用量'
+                                    FROM (
+                                    SELECT CMSMD.MD002 AS '線別',TA001 AS '製令單別',TA002 AS '製令單號',TA003 AS '開單日期',TA006 AS '產品品號',TA034 AS '產品品名',TA015 AS '預計產量',TA007 AS '單位1',TB003 AS '材料品號',TB012 AS '材料品名',TB007 AS '單位2',TB004 AS '需領用量',MC004 AS '標準批量',ROUND(TA015/MC004,3) AS '總桶數'
+                                    ,FLOOR(ROUND(TA015/MC004,3)) AS '整桶數'
+                                    ,(ROUND(TA015/MC004,3)-FLOOR(ROUND(TA015/MC004,3)))AS '最後桶數'
+                                    ,CASE WHEN FLOOR(ROUND(TA015/MC004,3)) = ROUND(TA015/MC004,3) THEN ROUND(TB004/ROUND(TA015/MC004,3),3) ELSE CASE WHEN (ROUND(TA015/MC004,3)-1)>0 THEN ROUND(TB004/ROUND(TA015/MC004,3),3) ELSE 0 END  END AS '整桶用量'
+                                    ,TB004-(ROUND(TB004/ROUND(TA015/MC004,3),3)*(CASE WHEN FLOOR(ROUND(TA015/MC004,3)) = ROUND(TA015/MC004,3) THEN ROUND(TA015/MC004,3) ELSE CASE WHEN (ROUND(TA015/MC004,3)-1)>0 THEN FLOOR(ROUND(TA015/MC004,3)) ELSE 0 END  END)) AS '最後桶用量'
+                                    ,ROUND(TB004/ROUND(TA015/MC004,3),3)  AS '標準用量'
+                                    FROM [TK].dbo.MOCTA,[TK].dbo.MOCTB,[TK].dbo.BOMMC,[TK].dbo.CMSMD
+                                    WHERE TA001=TB001 AND TA002=TB002
+                                    AND TA006=MC001
+                                    AND TA021=CMSMD.MD001
+                                    AND TB003 LIKE '1%'
+                                    AND TB003 NOT IN ('101001009')
+                                    AND CMSMD.MD002  IN ('新廠製一組','新廠製二組','新廠製三組(手工)') 
+                                     AND TA003>='{0}' AND TA003<='{1}'
+                                    ) AS TEMP
+                                    LEFT JOIN  [TK].dbo.BOMMD ON 產品品號=MD001 AND 材料品號=MD003
+                                    ORDER BY 線別,製令單別,製令單號"
                                 , dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
          
 
@@ -140,11 +147,11 @@ namespace TKWAREHOUSE
                                  AND TA006=MC001
                                  AND TA021=MD001
                                  AND TB003 LIKE '1%'
-                                 AND TB003 IN ('101001001','101001009')
+                                 AND TB003 IN ('101001009')
                                  AND MD002  IN ('新廠製一組','新廠製二組','新廠製三組(手工)') 
                                  AND TA003>='{0}' AND TA003<='{1}'
                                  GROUP BY MD002,TA003,TB003,TB012,TB007
-                                 ORDER BY MD002,TA003,TB003,TB012,TB007 
+                                 ORDER BY MD002,TA003,TB003,TB012,TB007  
                                 ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
 
             return FASTSQL.ToString();
