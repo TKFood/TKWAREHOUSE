318f568d813b8beb8883a2eb128e9eebe649266e
at3158 <tk290@tkfood.com.tw>
2021-09-29 15:47:49 +0800

    FrmREPORTMOC

--- TKWAREHOUSE/FrmREPORTMOC.cs
+++ TKWAREHOUSE/FrmREPORTMOC.cs
@@ -23,6 +23,8 @@ using NPOI.XSSF.UserModel;
 using System.Text.RegularExpressions;
 using FastReport;
 using FastReport.Data;
+using TKITDLL;
+
 
 namespace TKWAREHOUSE
 {
@@ -86,7 +88,18 @@ namespace TKWAREHOUSE
             report1 = new Report();
             report1.Load(@"REPORT\製令領用量.frx");
 
-            report1.Dictionary.Connections[0].ConnectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+            //20210902密
+            Class1 TKID = new Class1();//用new 建立類別實體
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+            //資料庫使用者密碼解密
+            sqlsb.Password = TKID.Decryption(sqlsb.Password);
+            sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+            String connectionString;
+            sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+            report1.Dictionary.Connections[0].ConnectionString = sqlsb.ConnectionString;
 
             TableDataSource Table = report1.GetDataSource("Table") as TableDataSource;
             TableDataSource Table1 = report1.GetDataSource("Table1") as TableDataSource;
