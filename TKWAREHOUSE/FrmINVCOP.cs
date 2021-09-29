dc146f17f91f2108f8bb8cf4e8a8845f5c7684a8
at3158 <tk290@tkfood.com.tw>
2021-09-29 15:17:05 +0800

    FrmINVCOP

--- TKWAREHOUSE/FrmINVCOP.cs
+++ TKWAREHOUSE/FrmINVCOP.cs
@@ -14,6 +14,7 @@ using System.Configuration;
 using NPOI.XSSF.UserModel;
 using NPOI.SS.Util;
 using System.Reflection;
+using TKITDLL;
 
 namespace TKWAREHOUSE
 {
@@ -71,8 +72,16 @@ namespace TKWAREHOUSE
             dtTemp2.Clear();
             try
             {
-                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
-                sqlConn = new SqlConnection(connectionString);
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
 
                 sbSql.Clear();
                 sbSqlQuery.Clear();
