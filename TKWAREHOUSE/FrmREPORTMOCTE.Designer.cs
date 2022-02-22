5ba3a378a9fabe277f73d64b3a9dbd6d30db85bc
at3158 <tk290@tkfood.com.tw>
2022-02-22 09:50:17 +0800

    SETFASETSQL

--- TKWAREHOUSE/FrmREPORTMOCTE.Designer.cs
+++ TKWAREHOUSE/FrmREPORTMOCTE.Designer.cs
@@ -152,7 +152,7 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(114, 16);
             this.label3.TabIndex = 2;
-            this.label3.Text = "日期";
+            this.label3.Text = "製令開單日期";
             // 
             // label4
             // 
