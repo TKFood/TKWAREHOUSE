a6d1c89be9e07a024ba14d432d1f8f55aeffc1f5
at3158 <tk290@tkfood.com.tw>
2025-07-04 10:57:46 +0800

    SETFASTREPORT

--- TKWAREHOUSE/FrmREPORTMOC.cs
+++ TKWAREHOUSE/FrmREPORTMOC.cs
@@ -81,8 +81,7 @@ namespace TKWAREHOUSE
         }
 
         public void SETFASTREPORT()
-        {
-
+        { 
             string SQL;
             string SQL1;
             report1 = new Report();
@@ -116,31 +115,71 @@ namespace TKWAREHOUSE
         {
             StringBuilder FASTSQL = new StringBuilder();
             StringBuilder STRQUERY = new StringBuilder();
+            StringBuilder STRQUERYNOTIN = new StringBuilder();
 
-          
+            //限定品號
+            DataTable DT = FIND_FrmREPORTMOC();
+            if(DT!=null && DT.Rows.Count>=1)
+            {
+                // 組合 SQL 條件字串             
+                STRQUERY.Append("AND (");
+
+                for (int i = 0; i < DT.Rows.Count; i++)
+                {
+                    string TB003 = DT.Rows[i]["TB003"].ToString();
+                    STRQUERY.AppendFormat("TB003 LIKE '{0}%'", TB003);
+
+                    if (i < DT.Rows.Count - 1)
+                    {
+                        STRQUERY.Append(" OR ");
+                    }
+                }
+
+                STRQUERY.Append(")");
+            }
+            //NOT IN 品號
+            DataTable DTNOTIN = FIND_FrmREPORTMOCNOTIN();
+            if (DTNOTIN != null && DTNOTIN.Rows.Count >= 1)
+            {
+                STRQUERYNOTIN.Append("AND TB003 NOT IN (");
+
+                for (int i = 0; i < DTNOTIN.Rows.Count; i++)
+                {
+                    string TB003 = DTNOTIN.Rows[i]["TB003"].ToString();
+                    STRQUERYNOTIN.AppendFormat("'{0}'", TB003);
+
+                    if (i < DTNOTIN.Rows.Count - 1)
+                    {
+                        STRQUERYNOTIN.Append(", ");
+                    }
+                }
+
+                STRQUERYNOTIN.Append(")");
+            }
+
             FASTSQL.AppendFormat(@"    
-                                    SELECT 線別,製令單別,製令單號,開單日期,產品品號,產品品名,預計產量,單位1,材料品號,材料品名,單位2,需領用量,標準批量,總桶數,整桶數,最後桶數,整桶用量,最後桶用量,標準用量
-                                    ,ISNULL(BOMMD.MD004,'') AS '材料單位',ISNULL(BOMMD.MD006,0) AS '組成用量',ISNULL(BOMMD.MD007,0) AS '底數',ISNULL(BOMMD.MD008,0) AS '損耗率%'
-                                    ,CASE WHEN ISNULL(BOMMD.MD006,0)>0 THEN ISNULL(BOMMD.MD006,0)/ISNULL(BOMMD.MD007,0)*(1+ISNULL(BOMMD.MD008,0)) ELSE 0 END AS 'BOM用量'
-                                    FROM (
-                                    SELECT CMSMD.MD002 AS '線別',TA001 AS '製令單別',TA002 AS '製令單號',TA003 AS '開單日期',TA006 AS '產品品號',TA034 AS '產品品名',TA015 AS '預計產量',TA007 AS '單位1',TB003 AS '材料品號',TB012 AS '材料品名',TB007 AS '單位2',TB004 AS '需領用量',MC004 AS '標準批量',ROUND(TA015/MC004,3) AS '總桶數'
-                                    ,(CASE WHEN TA015>0 AND MC004>0 THEN  FLOOR(ROUND(TA015/MC004,3)) ELSE 0 END) AS '整桶數'
-                                    ,(CASE WHEN TA015>0 AND MC004>0 THEN (ROUND(TA015/MC004,3)-FLOOR(ROUND(TA015/MC004,3)))   ELSE 0 END) AS '最後桶數'
-                                    ,(CASE WHEN TA015>0 AND MC004>0 THEN (CASE WHEN FLOOR(ROUND(TA015/MC004,3)) = ROUND(TA015/MC004,3) THEN ROUND(TB004/ROUND(TA015/MC004,3),3) ELSE CASE WHEN (ROUND(TA015/MC004,3)-1)>0 THEN ROUND(TB004/ROUND(TA015/MC004,3),3) ELSE 0 END  END )  ELSE 0 END)  AS '整桶用量'
-                                    ,(CASE WHEN TA015>0 AND MC004>0 THEN (TB004-(ROUND(TB004/ROUND(TA015/MC004,3),3)*(CASE WHEN FLOOR(ROUND(TA015/MC004,3)) = ROUND(TA015/MC004,3) THEN ROUND(TA015/MC004,3) ELSE CASE WHEN (ROUND(TA015/MC004,3)-1)>0 THEN FLOOR(ROUND(TA015/MC004,3)) ELSE 0 END  END)))  ELSE 0 END)   AS '最後桶用量'
-                                    ,(CASE WHEN TA015>0 AND MC004>0 THEN ROUND(TB004/ROUND(TA015/MC004,3),3) ELSE 0 END )  AS '標準用量'
-                                    FROM [TK].dbo.MOCTA,[TK].dbo.MOCTB,[TK].dbo.BOMMC,[TK].dbo.CMSMD
-                                    WHERE TA001=TB001 AND TA002=TB002
-                                    AND TA006=MC001
-                                    AND TA021=CMSMD.MD001
-                                    AND (TB003 LIKE '1%' OR TB003 LIKE '30101%')
-                                    AND TB003 NOT IN ('101001009')
-                                    AND CMSMD.MD002  IN ('製一線','製二線 ','手工線') 
-                                     AND TA003>='{0}' AND TA003<='{1}'
-                                    ) AS TEMP
-                                    LEFT JOIN  [TK].dbo.BOMMD ON 產品品號=MD001 AND 材料品號=MD003
-                                    ORDER BY 線別,製令單別,製令單號   "
-                                , dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+                                SELECT 線別,製令單別,製令單號,開單日期,產品品號,產品品名,預計產量,單位1,材料品號,材料品名,單位2,需領用量,標準批量,總桶數,整桶數,最後桶數,整桶用量,最後桶用量,標準用量
+                                ,ISNULL(BOMMD.MD004,'') AS '材料單位',ISNULL(BOMMD.MD006,0) AS '組成用量',ISNULL(BOMMD.MD007,0) AS '底數',ISNULL(BOMMD.MD008,0) AS '損耗率%'
+                                ,CASE WHEN ISNULL(BOMMD.MD006,0)>0 THEN ISNULL(BOMMD.MD006,0)/ISNULL(BOMMD.MD007,0)*(1+ISNULL(BOMMD.MD008,0)) ELSE 0 END AS 'BOM用量'
+                                FROM (
+                                SELECT CMSMD.MD002 AS '線別',TA001 AS '製令單別',TA002 AS '製令單號',TA003 AS '開單日期',TA006 AS '產品品號',TA034 AS '產品品名',TA015 AS '預計產量',TA007 AS '單位1',TB003 AS '材料品號',TB012 AS '材料品名',TB007 AS '單位2',TB004 AS '需領用量',MC004 AS '標準批量',ROUND(TA015/MC004,3) AS '總桶數'
+                                ,(CASE WHEN TA015>0 AND MC004>0 THEN  FLOOR(ROUND(TA015/MC004,3)) ELSE 0 END) AS '整桶數'
+                                ,(CASE WHEN TA015>0 AND MC004>0 THEN (ROUND(TA015/MC004,3)-FLOOR(ROUND(TA015/MC004,3)))   ELSE 0 END) AS '最後桶數'
+                                ,(CASE WHEN TA015>0 AND MC004>0 THEN (CASE WHEN FLOOR(ROUND(TA015/MC004,3)) = ROUND(TA015/MC004,3) THEN ROUND(TB004/ROUND(TA015/MC004,3),3) ELSE CASE WHEN (ROUND(TA015/MC004,3)-1)>0 THEN ROUND(TB004/ROUND(TA015/MC004,3),3) ELSE 0 END  END )  ELSE 0 END)  AS '整桶用量'
+                                ,(CASE WHEN TA015>0 AND MC004>0 THEN (TB004-(ROUND(TB004/ROUND(TA015/MC004,3),3)*(CASE WHEN FLOOR(ROUND(TA015/MC004,3)) = ROUND(TA015/MC004,3) THEN ROUND(TA015/MC004,3) ELSE CASE WHEN (ROUND(TA015/MC004,3)-1)>0 THEN FLOOR(ROUND(TA015/MC004,3)) ELSE 0 END  END)))  ELSE 0 END)   AS '最後桶用量'
+                                ,(CASE WHEN TA015>0 AND MC004>0 THEN ROUND(TB004/ROUND(TA015/MC004,3),3) ELSE 0 END )  AS '標準用量'
+                                FROM [TK].dbo.MOCTA,[TK].dbo.MOCTB,[TK].dbo.BOMMC,[TK].dbo.CMSMD
+                                WHERE TA001=TB001 AND TA002=TB002
+                                AND TA006=MC001
+                                AND TA021=CMSMD.MD001
+                                {2}
+                                {3}
+                                AND CMSMD.MD002  IN ('製一線','製二線 ','手工線') 
+                                    AND TA003>='{0}' AND TA003<='{1}'
+                                ) AS TEMP
+                                LEFT JOIN  [TK].dbo.BOMMD ON 產品品號=MD001 AND 材料品號=MD003
+                                ORDER BY 線別,製令單別,製令單號   "
+                            , dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"), STRQUERY.ToString(), STRQUERYNOTIN.ToString());
          
 
             return FASTSQL.ToString();
@@ -152,6 +191,47 @@ namespace TKWAREHOUSE
         {
             StringBuilder FASTSQL = new StringBuilder();
             StringBuilder STRQUERY = new StringBuilder();
+            StringBuilder STRQUERYNOTIN = new StringBuilder();
+
+            //限定品號
+            DataTable DT = FIND_FrmREPORTMOC();
+            if (DT != null && DT.Rows.Count >= 1)
+            {
+                // 組合 SQL 條件字串             
+                STRQUERY.Append("AND (");
+
+                for (int i = 0; i < DT.Rows.Count; i++)
+                {
+                    string TB003 = DT.Rows[i]["TB003"].ToString();
+                    STRQUERY.AppendFormat("TB003 LIKE '{0}%'", TB003);
+
+                    if (i < DT.Rows.Count - 1)
+                    {
+                        STRQUERY.Append(" OR ");
+                    }
+                }
+
+                STRQUERY.Append(")");
+            }
+            //NOT IN 品號
+            DataTable DTNOTIN = FIND_FrmREPORTMOCNOTIN();
+            if (DTNOTIN != null && DTNOTIN.Rows.Count >= 1)
+            {
+                STRQUERYNOTIN.Append("AND TB003 NOT IN (");
+
+                for (int i = 0; i < DTNOTIN.Rows.Count; i++)
+                {
+                    string TB003 = DTNOTIN.Rows[i]["TB003"].ToString();
+                    STRQUERYNOTIN.AppendFormat("'{0}'", TB003);
+
+                    if (i < DTNOTIN.Rows.Count - 1)
+                    {
+                        STRQUERYNOTIN.Append(", ");
+                    }
+                }
+
+                STRQUERYNOTIN.Append(")");
+            }
 
             FASTSQL.AppendFormat(@"  
                                  SELECT MD002 AS '線別',TA003 AS '開單日期',TB003 AS '材料品號',TB012 AS '材料品名',TB007 AS '單位2',SUM(TB004) AS '需領用量',CEILING(SUM(TB004)/22) AS '包數'
@@ -159,15 +239,128 @@ namespace TKWAREHOUSE
                                  WHERE TA001=TB001 AND TA002=TB002
                                  AND TA006=MC001
                                  AND TA021=MD001
-                                 AND (TB003 LIKE '1%' OR TB003 LIKE '30101%')
-                                 AND TB003 IN ('101001009')
+                                 {2}
+                                    {3}
                                  AND MD002  IN ('製一線','製二線 ','手工線') 
                                  AND TA003>='{0}' AND TA003<='{1}'
                                  GROUP BY MD002,TA003,TB003,TB012,TB007
                                  ORDER BY MD002,TA003,TB003,TB012,TB007  
-                                ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+                                ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"), STRQUERY.ToString(), STRQUERYNOTIN.ToString());
 
             return FASTSQL.ToString();
+        }
+
+
+        public DataTable FIND_FrmREPORTMOC()
+        {
+            DataSet DS1 = new DataSet();
+            SqlDataAdapter adapter1 = new SqlDataAdapter();
+            SqlCommandBuilder sqlCmdBuilder1 = new SqlCommandBuilder();
+
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+
+                sbSql.AppendFormat(@" 
+                                   SELECT [TB003]
+                                    FROM [TKWAREHOUSE].[dbo].[FrmREPORTMOC]
+                                    ");
+
+                adapter1 = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder1 = new SqlCommandBuilder(adapter1);
+                sqlConn.Open();
+                DS1.Clear();
+                adapter1.Fill(DS1, "DS1");
+                sqlConn.Close();
+
+
+                if (DS1.Tables["DS1"].Rows.Count >= 1)
+                {
+                    return DS1.Tables["DS1"];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch(Exception EX)
+            {
+                return null;
+            }
+            finally
+            {
+
+            }
+        }
+
+        public DataTable FIND_FrmREPORTMOCNOTIN()
+        {
+            DataSet DS1 = new DataSet();
+            SqlDataAdapter adapter1 = new SqlDataAdapter();
+            SqlCommandBuilder sqlCmdBuilder1 = new SqlCommandBuilder();
+
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+
+                sbSql.AppendFormat(@" 
+                                   SELECT [TB003]
+                                    FROM [TKWAREHOUSE].[dbo].[FrmREPORTMOCNOTIN]
+                                    ");
+
+                adapter1 = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder1 = new SqlCommandBuilder(adapter1);
+                sqlConn.Open();
+                DS1.Clear();
+                adapter1.Fill(DS1, "DS1");
+                sqlConn.Close();
+
+
+                if (DS1.Tables["DS1"].Rows.Count >= 1)
+                {
+                    return DS1.Tables["DS1"];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception EX)
+            {
+                return null;
+            }
+            finally
+            {
+
+            }
         }
 
         #endregion
